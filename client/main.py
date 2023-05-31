3e5338dc2054d76cc3e72ff67a2cc5296f6a736a
Ahmed Mohsen Ahmed <104008258+Mohyyy@users.noreply.github.com>
2023-05-31 17:37:56 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,8 +1,9 @@
-
 import pygame as pg
 from pygame_gui import UIManager, elements, UI_BUTTON_PRESSED, UI_TEXT_ENTRY_FINISHED
+
 from GameProxy import Game
 from Protocols import *
+
 
 def main():
     # Initialize pygame
@@ -11,36 +12,41 @@ def main():
     clock = pg.time.Clock()
 
     # Initialize pygame_gui
-    ui = UIManager(WINDOW_SIZE)
-    chat_text_box = elements.UITextBox(
-        relative_rect=pg.Rect((GAME_SIZE[0], GAME_SIZE[1] - CTB_HIGHT - ETB_HIGHT - BTN_HIGHT), (UI_SIZE, CTB_HIGHT)),
-        html_text="2D RACING",
-        manager=ui,
+    ui_manager = UIManager(WINDOW_SIZE)
+    chat_box = elements.UITextBox(
+        relative_rect=pg.Rect(
+            (GAME_SIZE[0], GAME_SIZE[1] - CTB_HIGHT - ETB_HIGHT - BTN_HIGHT),
+            (UI_SIZE, CTB_HIGHT),
+        ),
+        html_text="Welcome ASU 2D Car Racer 2023",
+        manager=ui_manager,
     )
-    entry_text_box = elements.UITextEntryLine(
-        relative_rect=pg.Rect((GAME_SIZE[0], GAME_SIZE[1] - BTN_HIGHT - ETB_HIGHT), (UI_SIZE, ETB_HIGHT)),
-        placeholder_text="Chat",
-        manager=ui,
+    text_entry = elements.UITextEntryLine(
+        relative_rect=pg.Rect(
+            (GAME_SIZE[0], GAME_SIZE[1] - BTN_HIGHT - ETB_HIGHT), (UI_SIZE, ETB_HIGHT)
+        ),
+        placeholder_text="Chat with Friends!",
+        manager=ui_manager,
     )
-    send_button = elements.UIButton(
-        relative_rect=pg.Rect((GAME_SIZE[0], GAME_SIZE[1] - BTN_HIGHT), (UI_SIZE, BTN_HIGHT)),
-        text="send",
-        manager=ui,
+    send_btn = elements.UIButton(
+        relative_rect=pg.Rect(
+            (GAME_SIZE[0], GAME_SIZE[1] - BTN_HIGHT), (UI_SIZE, BTN_HIGHT)
+        ),
+        text="send msg",
+        manager=ui_manager,
     )
 
     # Initialize Game
-    game = Game()
+    game_instance = Game()
 
     # Game Loop
     running = True
-    movement = [0, 0]  # Direction and Angle
+    movement: Movement = [0, 0]  # Direction and Angle
     while running:
-        time_delta = clock.tick(60) / 1000.0
-
+        delta_time = clock.tick(60) / 1000.0
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-
             if event.type == pg.KEYDOWN:
                 if event.key in (pg.K_UP, pg.K_w):
                     movement[0] = 1
@@ -50,24 +56,25 @@ def main():
                     movement[1] = 1
                 if event.key in (pg.K_RIGHT, pg.K_a):
                     movement[1] = -1
-
             if event.type == pg.KEYUP:
                 if event.key in (pg.K_UP, pg.K_w, pg.K_DOWN, pg.K_s):
                     movement[0] = 0
                 if event.key in (pg.K_LEFT, pg.K_d, pg.K_RIGHT, pg.K_a):
                     movement[1] = 0
+            if event.type == UI_BUTTON_PRESSED:
+                if event.ui_element == send_btn:
+                    msg_text = text_entry.get_text()
+                    text_entry.set_text("")
+                    chat_box.append_html_text(msg_text + "\n")
+            if event.type == UI_TEXT_ENTRY_FINISHED:
+                msg_text = text_entry.get_text()
+                text_entry.set_text("")
+                chat_box.append_html_text(msg_text + "\n")
+            ui_manager.process_events(event)
 
-            if event.type == UI_BUTTON_PRESSED or event.type == UI_TEXT_ENTRY_FINISHED:
-                if event.ui_element == send_button:
-                    text = entry_text_box.get_text()
-                    entry_text_box.set_text("")
-                    chat_text_box.append_html_text(text + "\n")
-
-            ui.process_events(event)
-
-        game.update(screen, movement)
-        ui.update(time_delta)
-        ui.draw_ui(screen)
+        game_instance.update(screen, movement)
+        ui_manager.update(delta_time)
+        ui_manager.draw_ui(screen)
         pg.display.update()
 
     pg.quit()
@@ -75,6 +82,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-```
-
-Note: I assumed that the constant variables `WINDOW_SIZE`, `GAME_SIZE`, `CTB_HIGHT`, `ETB_HIGHT`, `BTN_HIGHT`, and `UI_SIZE` are defined elsewhere in the code and remain unchanged.
